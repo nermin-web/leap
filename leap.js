662e2e5a68430a81c7db6ccfc6bebee5075ebe8c
nermin-web <nerminbajric@gmail.com>
2020-07-30 23:56:51 +0200

    Zero fix

--- leap.js
+++ leap.js
@@ -1,6 +1,6 @@
 let year = 2020;
 
-if (year % 400 == 0 || year % 4 == 0 || year % 100 == 1) {
+if (year % 400 == 0 || year % 4 == 0 || year % 100 != 0) {
     console.log('It is a leap year.');
 } else {
     console.log('It is not a leap year.');
