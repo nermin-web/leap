10b9c9bab380b2563dbac8eb1a93a4eeb289ba69
nermin-web <nerminbajric@gmail.com>
2020-07-30 21:11:45 +0200

    First commit

---
+++ leap.js
@@ -0,0 +1,13 @@
+let year = 1946;
+
+if (year % 400 == 0) {
+    console.log('It is a leap year.');
+} else if (year % 100 == 0) {
+    console.log('It is definitely not a leap year.');
+} else if (year % 4 == 0) {
+    console.log('It is definitely a leap year.');
+} else {
+    console.log('It is not a leap year.');
+}
+
+
